92b6f5ab31b6618527c7f614ccf750b442836538
olehdovhan <81869684+olehdovhan@users.noreply.github.com>
2021-06-09 21:44:06 +0300

    Create repository

---
+++ RentCars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentCars
+//
+//  Created by Oleh Dovhan on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
