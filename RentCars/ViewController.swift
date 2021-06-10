7fc760b300b8ce6e6dd07064f5ef27d4cc159b29
olehdovhan <81869684+olehdovhan@users.noreply.github.com>
2021-06-10 11:40:04 +0300

    Add referencing outlets and sent events to ViewController

--- RentCars/ViewController.swift
+++ RentCars/ViewController.swift
@@ -9,6 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet var markLabel: UILabel!
+  
+  @IBOutlet var modelLabel: UILabel!
+  
+  @IBOutlet var carImageView: UIImageView!
+  
+  @IBOutlet var lastTimeStartedLabel: UILabel!
+  
+  @IBOutlet var numberOfTripsLabel: UILabel!
+  
+  @IBOutlet var ratingLabel: UILabel!
+  
+  @IBOutlet var segmentedControl: UISegmentedControl!
+  
+
+  @IBAction func segmentedControlPressed(_ sender: UISegmentedControl) {
+  }
+  
+  @IBAction func startTripPressed(_ sender: UIButton) {
+  }
+  
+  @IBAction func ratePressed(_ sender: UIButton) {
+  }
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
